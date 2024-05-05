a36526820a8fbfe0066b5397b96cf67e3b96f449
saetgit <etemadian.sara.1998@gmail.com>
2024-05-05 10:01:28 +0330

    POST request

--- server/api/todo/index.ts
+++ server/api/todo/index.ts
@@ -1,8 +1,22 @@
 import { db } from "../../db";
+import { v4 as uuid } from "uuid";
 export default defineEventHandler((e) => {
-  console.log({ e });
   const method=e.req.method;
   if(method ==="GET"){
     return db.todos;
+  }
+
+  if(method==="POST"){
+    const body=await useBody(e);
+
+    if(!body.item) throw new  Error();
+
+        const newTodo={
+            id:uuid(),
+            item:body.item,
+            completed:false
+        };
+        db.todos.push(newTodo)
+        return newTodo
   }
 });
