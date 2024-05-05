d4b0de9bc91c9c1e392fc35102329837742457f7
saetgit <etemadian.sara.1998@gmail.com>
2024-05-05 10:24:18 +0330

    PUT and DELETE request

---
+++ server/api/todo/[id].ts
@@ -0,0 +1,40 @@
+import { db } from "../../db";
+export default defineEventHandler((e) => {
+  const method = e.req.method;
+  const context = e.context;
+    const { id } = context.params;
+
+  const findTodoById=(todoId)=>{
+    if (method === "PUT") {
+        let index;
+        const todo = db.todos.find((t, i) => {
+          if (t.id === todoId) {
+            index = i;
+            return true;
+          }
+          return false;
+        });
+    
+        if (!todo) throw new Error();
+    return todo,index
+  }
+  if (method === "PUT") {
+    
+const {todo,index}=findTodoById(id)
+    const updateTodo = {
+      ...todo,
+      completed:!todo.completed,
+    };
+
+    db.todos[index] = updateTodo;
+    return updateTodo;
+  }
+
+  if(method==="DELETE"){
+    const{todo,index} =findTodoById(id);
+    db.todos.splice(index,1);
+ return{
+    message:"item deleted"
+    }
+  }
+});
